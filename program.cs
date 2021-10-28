dc1166856f1b91e359cca349172c275c429d0d2a
kunall-ll <kunal.agarwala6@gmail.com>
2021-10-28 18:13:39 +0100

    Fixed Schema

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SalesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
