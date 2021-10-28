8dd123eb2174f456b4d2ae300edd4ee05586b072
kunall-ll <kunal.agarwala6@gmail.com>
2021-10-28 18:23:56 +0100

    insert query function

--- program.cs
+++ program.cs
@@ -8,7 +8,27 @@ namespace SalesProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            switch (userinput)
+            {
+                case "Data Entry":
+                    insert();
+                    break;
+                case "Reports":
+                    select();
+                    break;
+                default:
+                    Console.WriteLine("Enter your choice again");
+                    break;
+            }
+
+            void insert()
+            {
+                string insertquery = "INSERT INTO sale (ProductName, Quantity, Price, SaleDate) VALUES('Air Fryer', 56, 7.99, '2008-11-15')";
+
+                sendCommand(insertquery);
+
+
+            }
         }
     }
 }
